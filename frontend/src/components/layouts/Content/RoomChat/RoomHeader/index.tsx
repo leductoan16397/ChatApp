8aae52aaec8bcc1c0bb4db632702fb043fd8a81b
hiep doan <hiepdoan.cmcglobal@gmail.com>
2021-10-26 17:35:48 +0700

    update

--- frontend/src/components/layouts/Content/RoomChat/RoomHeader/index.tsx
+++ frontend/src/components/layouts/Content/RoomChat/RoomHeader/index.tsx
@@ -1,6 +1,22 @@
 import React, { FC } from 'react';
 import './index.scss';
+import { Avatar } from '@mui/material';
+import { ActiveDot } from 'components/common/activeDot';
 
 export const RoomChatHeader: FC = () => {
-  return <div className="room-chat-header">header</div>;
+  return (
+    <div className="room-chat-header">
+      <div className="room-avatar-status">
+        <div className="room-avatar">
+          <Avatar sx={{ width: 50, height: 50 }}>H</Avatar>
+          <ActiveDot />
+        </div>
+        <div className="room-status-name">
+          <div className="room-name">room name room name room name room name</div>
+          <div className="room-status">status</div>
+        </div>
+      </div>
+      <div className="room-action">call</div>
+    </div>
+  );
 };
