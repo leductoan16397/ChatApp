f0dafd44bd43326fa3e15dc9959ab2a48132d646
toanleduc <leductoan16397@gmail.com>
2021-11-01 17:40:43 +0700

    update

--- frontend/src/components/layouts/Content/RoomChat/InputChat/index.tsx
+++ frontend/src/components/layouts/Content/RoomChat/InputChat/index.tsx
@@ -1,6 +1,35 @@
 import React, { FC } from 'react';
+import { BsCardImage, BsFillStickyFill } from 'react-icons/bs';
+import { IoMdCloseCircle } from 'react-icons/io';
+import { RiFileGifFill } from 'react-icons/ri';
 import './index.scss';
 
 export const ChatInput: FC = () => {
-  return <div className="chat-input">input</div>;
+  return (
+    <div className="chat-input">
+      <div className="input-chat-gr">
+        <div className="more-action">
+          <div className="input-mess-icon">
+            {/* <FaPlusCircle size="20px"  color="blue" /> */}
+            <IoMdCloseCircle size="20px" color="blue" />
+          </div>
+        </div>
+        <div className="input-message-group">
+          <div className="input-mes-icon-gr">
+            <div className="input-mess-icon">
+              <BsCardImage size="20px" color="blue" />
+            </div>
+            <div className="input-mess-icon">
+              <BsFillStickyFill size="20px" color="blue" />
+            </div>
+            <div className="input-mess-icon">
+              <RiFileGifFill size="20px" color="blue" />
+            </div>
+          </div>
+          <div className="input-mes-text">input</div>
+        </div>
+        <div className="send-like">send</div>
+      </div>
+    </div>
+  );
 };
--- frontend/src/components/layouts/Content/RoomChat/RoomHeader/index.tsx
+++ frontend/src/components/layouts/Content/RoomChat/RoomHeader/index.tsx
@@ -2,13 +2,16 @@ import React, { FC } from 'react';
 import './index.scss';
 import { Avatar } from '@mui/material';
 import { ActiveDot } from 'components/common/activeDot';
+import { IoIosCall, IoIosVideocam } from 'react-icons/io';
+// import { FaVideo } from 'react-icons/fa';
+import { BsThreeDots } from 'react-icons/bs';
 
 export const RoomChatHeader: FC = () => {
   return (
     <div className="room-chat-header">
       <div className="room-avatar-status">
         <div className="room-avatar">
-          <Avatar sx={{ width: 50, height: 50 }}>H</Avatar>
+          <Avatar sx={{ width: 40, height: 40 }}>H</Avatar>
           <ActiveDot />
         </div>
         <div className="room-status-name">
@@ -16,7 +19,22 @@ export const RoomChatHeader: FC = () => {
           <div className="room-status">status</div>
         </div>
       </div>
-      <div className="room-action">call</div>
+      <div className="room-action">
+        <div className="room-action-icon">
+          <IoIosCall size="25px" color="blue" />
+        </div>
+        <div className="room-action-icon">
+          <IoIosVideocam size="25px" color="blue" />
+        </div>
+        <div className="room-action-icon">
+          <BsThreeDots size="20px" color="blue" />
+        </div>
+        {/* <div className="room-action-icon">
+          <div className="room-info-icon-active" style={{ backgroundColor: 'blue' }}>
+            <BsThreeDots size="20px" color="white" />
+          </div>
+        </div> */}
+      </div>
     </div>
   );
 };
